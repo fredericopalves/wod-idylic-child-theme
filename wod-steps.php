8eb3f4624d81a374d179f1e3b208261cf71dd014
FREDERICO ALVES <frederico.alves@ericsson.com>
2018-06-29 11:38:02 -0500

    another commit

--- wod-steps.php
+++ wod-steps.php
@@ -1,9 +1,9 @@
 <div class='wod-four-column'>
  
-<a href= "/wod/learn-yourself/" class="wod-column-4 btn ">Learn Yourself</a>   
- <a href="/wod/launch-your-mission/" class="wod-column-4 btn ">Launch Your Mission</a>  
- <a href="/wod/live-with-purpose/" class="wod-column-4 btn ">Live with Purpose</a>  
- <a href="/wod/love-forward/" class="wod-column-4 btn " >Love Forward</a>  
+<a href= "/~desti518/wod/learn-yourself/" class="wod-column-4 btn ">Learn Yourself</a>   
+ <a href="/~desti518/wod/launch-your-mission/" class="wod-column-4 btn ">Launch Your Mission</a>  
+ <a href="/~desti518/wod/live-with-purpose/" class="wod-column-4 btn ">Live with Purpose</a>  
+ <a href="/~desti518/wod/love-forward/" class="wod-column-4 btn " >Love Forward</a>  
 
 
 
